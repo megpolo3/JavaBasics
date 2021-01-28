8f7b07d9125d2d7b334c5ac21a52ec688ece4418
Megha Garg <Megha_Garg@gap.com>
2021-01-28 10:00:06 -0500

    parametrized enums

--- src/main/java/enums.java
+++ src/main/java/enums.java
@@ -17,9 +17,41 @@ public class enums {
 
     }
 
+    //parametrized constructor
+    enum TrafficSignal
+    {
+        // This will call enum constructor with one
+        // String argument
+        RED("STOP"), GREEN("GO"), ORANGE("SLOW DOWN");
+
+        // declaring private variable for getting values
+        private String action;
+
+        // getter method
+        public String getAction()
+        {
+            return this.action;
+        }
+
+        // enum constructor - cannot be public or protected
+        private TrafficSignal(String action)
+        {
+            this.action = action;
+        }
+    }
+
     public static void main(String args[]){
         Color c = Color.RED;
         System.out.println(c);
+
+        TrafficSignal[] signals = TrafficSignal.values();
+
+        for (TrafficSignal signal : signals)
+        {
+            // use getter method to get the value
+            System.out.println("name : " + signal.name() +
+                    " action: " + signal.getAction() );
+        }
     }
 
 }
